8b7958a32d2e0fff2828a6f95610726ab3c380f8
Acrom07 <47199542+Acrom07@users.noreply.github.com>
2019-02-19 19:50:33 -0600

    Practica de controles de validacion

---
+++ Tarea 2/Validar.php
@@ -0,0 +1,14 @@
+<?php
+
+mb_internal_encoding('UTF-8');
+
+$nombre = $_POST[ 'Nombre'];
+$contraseña = $_POST[ 'Pass'];
+$email = $_POST[ 'Correo'];
+$fnacimiento = $_POST[ 'Fnacimiento'];
+$comodin = $_POST[ 'Comodin'];
+
+echo "El formulario tiene los datos", '', $ Nombre;
+
+
+ ?>
